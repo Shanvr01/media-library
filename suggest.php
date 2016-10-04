bd70745f15b6f25f1babc3fd42a511c3a0b54d70
Shanvr01 <shan.v.rensburg@gmail.com>
2016-10-04 08:34:44 +0200

    Added form

--- suggest.php
+++ suggest.php
@@ -5,7 +5,29 @@
 ?>
 
 	<div class="page-header">
-		<h1><?php echo $pageTitle; ?></h1>
+		<div class="section page">
+			<div class="wrapper">
+				<h1><?php echo $pageTitle; ?></h1>
+				<p>If you think there is something I&rsquo;m missing, let me know! Complete the form to send me an email.</p>
+				<form method="post" action="process.php">
+					<table>
+						<tr>
+							<th><label for="name">Name</label></th>
+							<td><input type="text" name="name" id="name" /></td>
+						</tr>
+						<tr>
+							<th><label for="email">Email</label></th>
+							<td><input type="email" name="email" id="email" /></td>
+						</tr>
+						<tr>
+							<th><label for="details">Suggest Item Details</label></th>
+							<td><textarea name="details" id="details"></textarea></td>
+						</tr>
+					</table>
+					<input type="submit" value="send"/>
+				</form>
+			</div>
+		</div>
 	</div>
 
 <?php

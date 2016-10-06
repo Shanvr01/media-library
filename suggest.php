4b0e0a45b5cc0bd777542e0811c8e0bd38dda927
Shanvr01 <shan.v.rensburg@gmail.com>
2016-10-06 08:21:43 +0200

    added email function

--- suggest.php
+++ suggest.php
@@ -1,25 +1,54 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	$name = $_POST["name"];
-	$email = $_POST["email"];
-	$details = $_POST["details"];
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+		$name = trim(filter_input(INPUT_POST,"name", FILTER_SANITIZE_STRING));
+		$email = trim(filter_input(INPUT_POST,"email", FILTER_SANITIZE_STRING));
+		$details = trim(filter_input(INPUT_POST,"details", FILTER_SANITIZE_SPECIAL_CHARS));
 
-	echo "<pre>";
-	$email_body = "";
-	$email_body .= "Name " . $name . "\n";
-	$email_body .= "Email " . $email . "\n";
-	$email_body .= "Details " . $details . "\n";
-	echo $email_body;
-	echo "</pre>";
+		if ($name == "" || $email == "" || $details == "") {
+			echo "Please fill in the required fields: Name, Email and Details";
+			exit();
+		}
+		if ($_POST["address"] != "") {
+			echo "Bad form input";
+			exit();
+		}
 
-	// To Do: Send Email
-	header('location:suggest.php?status=thanks');
-}
+		require('/includes/phpmailer/class.phpmailer.php');
 
-$pageTitle = 'Suggest Page';
-$section = 'suggest';
-include('/includes/header.php');
+		$mail = new PHPmailer;
+		if (!$mail->ValidateAddress($email)) {
+			echo "Invalid Email Address";
+			exit();
+		}
+
+
+
+		$email_body = "";
+		$email_body .= "Name " . $name . "\n";
+		$email_body .= "Email " . $email . "\n";
+		$email_body .= "Details " . $details . "\n";
+
+		$mail->setFrom($email, $name);
+		$mail->addAddress('treehouse@localhost', 'Shan');     // Add a recipient
+
+		$mail->isHTML(false);                                  // Set email format to HTML
+
+		$mail->Subject = 'Personal Media Library Suggestion from ' . $name;
+		$mail->Body    = $email_body;
+
+		if(!$mail->send()) {
+		    echo 'Message could not be sent.';
+		    echo 'Mailer Error: ' . $mail->ErrorInfo;
+		    exit();
+		}
+
+		header('location:suggest.php?status=thanks');
+	}
+
+	$pageTitle = 'Suggest Page';
+	$section = 'suggest';
+	include('/includes/header.php');
 
 ?>
 
@@ -27,7 +56,7 @@ include('/includes/header.php');
 		<div class="section page">
 			<div class="wrapper">
 				<h1><?php echo $pageTitle; ?></h1>
-				<?php if (isset ($_GET['status']) && $_GET['status'] == 'thanks') { 
+				<?php if (isset ($_GET['status']) && $_GET['status'] == 'thanks') {
 					echo "<p>Thanks for the email! I&rsquo;ll check out your suggestions shortly!</p>";
 				} else {
 				?>
@@ -40,11 +69,16 @@ include('/includes/header.php');
 						</tr>
 						<tr>
 							<th><label for="email">Email</label></th>
-							<td><input type="email" name="email" id="email" /></td>
+							<td><input type="text" name="email" id="email" /></td>
 						</tr>
 						<tr>
 							<th><label for="details">Suggest Item Details</label></th>
 							<td><textarea name="details" id="details"></textarea></td>
+						</tr>
+						<tr style = "display: none;">
+							<p style="display: none;>Please leave this field blank</p>
+							<th><label for="address">Address</label></th>
+							<td><input type="text" name="address" id="address" /></td>
 						</tr>
 					</table>
 					<input type="submit" value="send"/>

1ba9ab2d52ab827b277d85ee20fe3c83e874fcf9
Shanvr01 <shan.v.rensburg@gmail.com>
2016-10-07 09:00:38 +0200

    added additional form details

--- suggest.php
+++ suggest.php
@@ -3,47 +3,56 @@
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$name = trim(filter_input(INPUT_POST,"name", FILTER_SANITIZE_STRING));
 		$email = trim(filter_input(INPUT_POST,"email", FILTER_SANITIZE_STRING));
+		$category = trim(filter_input(INPUT_POST,"category", FILTER_SANITIZE_STRING));
+		$title = trim(filter_input(INPUT_POST,"title", FILTER_SANITIZE_STRING));
+		$format = trim(filter_input(INPUT_POST,"format", FILTER_SANITIZE_STRING));
+		$genre = trim(filter_input(INPUT_POST,"genre", FILTER_SANITIZE_STRING));
+		$year = trim(filter_input(INPUT_POST,"year", FILTER_SANITIZE_STRING));
 		$details = trim(filter_input(INPUT_POST,"details", FILTER_SANITIZE_SPECIAL_CHARS));
 
-		if ($name == "" || $email == "" || $details == "") {
-			echo "Please fill in the required fields: Name, Email and Details";
-			exit();
+		if ($name == "" || $email == "" || $category == "" || $title == "" ) {
+			$error_message = "Please fill in the required fields: Name, Email, Category and Title";
 		}
 		if ($_POST["address"] != "") {
-			echo "Bad form input";
-			exit();
+			$error_message = "Bad form input";
 		}
 
 		require('/includes/phpmailer/class.phpmailer.php');
 
 		$mail = new PHPmailer;
 		if (!$mail->ValidateAddress($email)) {
-			echo "Invalid Email Address";
-			exit();
+			$error_message = "Invalid Email Address";
 		}
 
+		if(!isset($error_message)) {
 
+			$email_body = "";
+			$email_body .= "Name " . $name . "\n";
+			$email_body .= "Email " . $email . "\n";
+			$email_body .= "Suggsted Item\n";
+			$email_body .= "Category " . $category . "\n";
+			$email_body .= "Title " . $title . "\n";
+			$email_body .= "Format " . $format . "\n";
+			$email_body .= "Genre " . $genre . "\n";
+			$email_body .= "Year " . $year . "\n";
+			$email_body .= "Details " . $details . "\n";
 
-		$email_body = "";
-		$email_body .= "Name " . $name . "\n";
-		$email_body .= "Email " . $email . "\n";
-		$email_body .= "Details " . $details . "\n";
+			$mail->setFrom($email, $name);
+			$mail->addAddress('treehouse@localhost', 'Shan');     // Add a recipient
 
-		$mail->setFrom($email, $name);
-		$mail->addAddress('treehouse@localhost', 'Shan');     // Add a recipient
+			$mail->isHTML(false);                                  // Set email format to HTML
 
-		$mail->isHTML(false);                                  // Set email format to HTML
+			$mail->Subject = 'Personal Media Library Suggestion from ' . $name;
+			$mail->Body    = $email_body;
 
-		$mail->Subject = 'Personal Media Library Suggestion from ' . $name;
-		$mail->Body    = $email_body;
-
-		if(!$mail->send()) {
-		    echo 'Message could not be sent.';
-		    echo 'Mailer Error: ' . $mail->ErrorInfo;
-		    exit();
+			if($mail->send()) {
+				header('location:suggest.php?status=thanks');
+				exit;
+			}
+			    $error_message = 'Message could not be sent.';
+			    $error_message .= 'Mailer Error: ' . $mail->ErrorInfo;
+			}
 		}
-
-		header('location:suggest.php?status=thanks');
 	}
 
 	$pageTitle = 'Suggest Page';
@@ -64,15 +73,60 @@
 				<form method="post" action="suggest.php">
 					<table>
 						<tr>
-							<th><label for="name">Name</label></th>
+							<th><label for="name">Name (required)</label></th>
 							<td><input type="text" name="name" id="name" /></td>
 						</tr>
 						<tr>
-							<th><label for="email">Email</label></th>
+							<th><label for="email">Email (required)</label></th>
 							<td><input type="text" name="email" id="email" /></td>
 						</tr>
 						<tr>
-							<th><label for="details">Suggest Item Details</label></th>
+							<th><label for="category">Category (required)</label></th>
+							<td>
+								<select name="category" id="category">
+									<option value="">Select One</option>
+									<option value="Books">Book</option>
+									<option value="Movies">Movie</option>
+									<option value="Music">Music</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<th><label for="title">Title (required)</label></th>
+							<td><input type="text" name="title" id="title" /></td>
+						</tr>
+						<tr>
+							<th><label for="format">Format</label></th>
+							<td>
+								<select name="format" id="format">
+									<option value="">Select One</option>
+									<optgroup label="Books">
+										<option value="Audio">Audio</option>
+										<option value="Ebook">Ebook</option>
+										<option value="Hardback">Hardback</option>
+										<option value="Paperback">Paperback</option>
+									</optgroup>
+									<optgroup label="Movies">
+										<option value="Blu-ray">Blu-ray</option>
+										<option value="DVD">DVD</option>
+										<option value="Streaming">Streaming</option>
+										<option value="VHS">VHS</option>
+									</optgroup>
+									<optgroup label="Music">
+										<option value="Cassette">Cassette</option>
+										<option value="CD">CD</option>
+										<option value="MP3">MP3</option>
+										<option value="Vinyl">Vinyl</option>
+									</optgroup>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<th><label for="year">Year</label></th>
+							<td><input type="text" name="year" id="year" /></td>
+						</tr>
+						<tr>
+							<th><label for="details">Additional Details</label></th>
 							<td><textarea name="details" id="details"></textarea></td>
 						</tr>
 						<tr style = "display: none;">
